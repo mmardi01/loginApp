72a57dc69118b3acfad9756733cf16389e8e77f8
Mustapha Mardi <mmardi@e2r5p7.1337.ma>
2023-05-20 23:35:33 +0100

    workin on password validation

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -131,7 +131,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "37e5a470c9b39a7f"; }
+/******/ 		__webpack_require__.h = function() { return "fba51963a7159644"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
--- src/pages/signup.tsx
+++ src/pages/signup.tsx
@@ -1,21 +1,32 @@
 import { useState } from "react";
+import { useRouter } from "next/router";
 import axios from "axios";
+
+
 export default function Signup() {
   const [uName, setUName] = useState("");
   const [password, setPassword] = useState("");
   const [cPassword, setCPassword] = useState("");
+  const [pError, setPError] = useState(false);
+  const [uError, setUError] = useState(false);
+  
+  const router = useRouter();
+
   const handleSubmit = (e: any) => {
     e.preventDefault()
 
-    if (!uName || !cPassword || !password)
-      console.log("error");
-    else if (password == cPassword) {
+    if (uName || !cPassword || !password)
+      setUError(true)
+    if (password == cPassword) {
       const data = {
         "uname": uName,
         "password": password,
       }
-      axios.post("#",data).then(()=>{
-        console.log("data sent");
+      axios.post("http://10.12.6.6:4000/user",data).then((respons)=>{
+        if (respons.data.userid !== 'false')
+          router.push("/login");
+        else
+          setUError(true)
       }).catch(()=>{
         console.log("data has not sent")
       })
@@ -26,28 +37,30 @@ export default function Signup() {
 
 
   return (
-    <div className=' border rounded-[30px] border-[#6c9df3] h-[400px] w-72 grid content-center'>
-      <form className='text-white  grid justify-items-center' onSubmit={handleSubmit}>
-        <label className='' htmlFor="username">Username</label>
+    <div className='shadow-xl bg-white border rounded-[30px] border-[#6c9df3] h-[400px] w-72 grid content-center'>
+      <form className='text-[#4f59e8]  grid justify-items-center' onSubmit={handleSubmit}>
+        {
+          !uError ? <label className='' htmlFor="username">Username</label>  : (uError && uName.length !== 0) ? <label className='text-red-500' htmlFor="username">Username already exist</label> : <label className='text-red-500' htmlFor="username">Username can't be empty</label>
+        }
         <input 
           onChange={(e)=> setUName(e.target.value) } 
-          className='placeholder-[#acacac] focus:outline-none text-white px-[60px] py-[10px] w-[200px] bg-transparent border rounded-[30px] border-[#6c9df3] my-2 p-[px]' 
+          className='placeholder-[#acacac] focus:outline-none text-black px-[60px] py-[10px] w-[200px] bg-transparent border rounded-[30px] border-[#6c9df3] my-2 ' 
           type="text" 
-          placeholder='Username' 
+          placeholder='username' 
         />
         <label className='my-1' htmlFor="username">Password</label>
         <input
           onChange={(e) => setPassword(e.target.value)}
-          className=' placeholder-[#acacac] focus:outline-none text-white px-[60px] py-[10px] w-[200px] bg-transparent border rounded-[30px] border-[#6c9df3] my-2 p-[px] ' 
+          className=' placeholder-[#acacac] focus:outline-none text-black px-[60px] py-[10px] w-[200px] bg-transparent border rounded-[30px] border-[#6c9df3] my-2 ' 
           type="password" 
           placeholder='password' 
         />
-        <label className='my-1' htmlFor="username">Password confirmation</label>
+        <label className='my-1 ' htmlFor="username">Password confirmation</label>
         <input
-           onChange={(e) => setCPassword(e.target.value)} 
-          className=' placeholder-[#acacac] focus:outline-none text-white px-[12px] py-[10px] w-[200px] bg-transparent border rounded-[30px] border-[#6c9df3] my-2 p-[px] ' 
+          onChange={(e) => setCPassword(e.target.value)} 
+          className=' placeholder-[#acacac] focus:outline-none text-black px-[60px] py-[10px] w-[200px] bg-transparent border rounded-[30px] border-[#6c9df3] my-2 ' 
           type="password" 
-          placeholder='Password confirmation' 
+          placeholder='password' 
         />
         <button className='mt-[30px] hover:text-[#4f59e8] tracking-wider bg-[#4f59e8] text-[hsl(0,0%,100%)] rounded-full px-5 py-2 hover:bg-[#ffffff]' type='submit'>Sign Up</button>
       </form>
